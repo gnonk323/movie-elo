ae9873ca3080ffdce70954a109a82af52fefcd24
gnonk323 <77447364+gnonk323@users.noreply.github.com>
2022-10-15 01:50:03 -0400

    added get_movies.py
    added get_movies.py, which queries the TMDB API and writes a given number of pages of movies and their corresponding posters to a csv file named movies_from_tmdb.csv

--- get_movies.py
+++ get_movies.py
@@ -1,17 +1,25 @@
+from importlib.metadata import packages_distributions
 import requests
 import pandas
 import requests
 
-response = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=a4c7d95ac45652f394bbbd1cd825f0dd&language=en-US&page=1")
+KEY = 'a4c7d95ac45652f394bbbd1cd825f0dd'
 
-top_rated = response.json()
+pages_to_retrieve = 10
 
-top_rated_films = top_rated['results']
-
-columns = ['film']
-
+columns = ['film', 'poster']
 df = pandas.DataFrame(columns=columns)
-for film in top_rated_films:
-    df.loc[len(df)]=film['title']
+
+for page in range(1, pages_to_retrieve+1):
+    response = requests.get(f"https://api.themoviedb.org/3/movie/top_rated?api_key={KEY}&language=en-US&page={page}")
+
+    top_rated = response.json()
+
+    top_rated_films = top_rated['results']
+
+    for film in top_rated_films:
+        df.loc[len(df)]=[film['title'], f'<img src="https://image.tmdb.org/t/p/w500/{film["poster_path"]}">']
 
 print(df)
+
+df.to_csv('movies_from_tmdb.csv')
